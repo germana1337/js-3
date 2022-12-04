36101247ea10d55352c10578084490b3cec3c02c
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2022-12-05 03:15:54 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,75 @@
+"use strict";
+// პირველი
+
+// let sum = 0;
+
+// function sumnums(...numbers){
+//     for (let i = 0; i < numbers.length; i++){
+//         if(numbers[i] >= 0){
+//             sum += numbers[i];
+//         }
+//     }
+//     return sum
+// }
+
+// sumnums (2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8);
+
+// მეორე
+
+// let sum = 0
+// function sumnum(...numbers){
+//     for(let i = 0; i < numbers.length; i++){
+//         sum += numbers[i]
+//     }
+//     return sum
+// }
+
+// sumnum (10, 50, 6, 7, 8, 11, 6, 3, 9);
+
+// მესამე
+
+// let user = {
+//     firstname: 'giorgi',
+//     lastname: 'saakadze',
+//     age: 32,
+//     isloggedin: true
+//   }
+
+//   function userInfo(){
+//     if(user.isloggedin == true){
+//         return (user.firstname + " " + user.lastname);
+//     } else {
+//         'false'
+//     }
+//   }
+//   userInfo();
+
+// მეოთხე
+
+// let maxnums = (...numbers) => {
+//     console.log(Math.max(...numbers));
+// }
+//  maxnums(120,,330,50,14,21,56,90);
+
+//
+
+// მეხუთე
+
+// let array = ["html", "css", "python", "javascript", "bootstrap"];
+
+// for (var i = 0; i < array.length; i++) {
+//   if (array[i].length > 4 && array[i].includes("av")) {
+//     console.log(array[i]);
+//   }
+// }
+
+
+// let array2 = ['html', 'css', 'python', 'javascript', 'bootstrap'];
+
+// array2.forEach(function(json) {
+//     if (json.length > 4 && json.includes("av")  )
+//     {
+//         console.log(json);
+//     }
+// }
+// )
